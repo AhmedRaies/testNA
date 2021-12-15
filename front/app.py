01a7f4477faf4e4021f0dfda197bc641b9f6af9b
AhmedRaies <ahmed.raies@ensit.u-tunis.tn>
2021-12-15 23:29:43 +0100

    pushing

--- front/app.py
+++ front/app.py
@@ -7,6 +7,5 @@ app=Flask(__name__, template_folder='templates')
 def upload_files():
    return render_template("form.html")
 
-		
-if __name__ == "__main__":
-   app.run(host="0.0.0.0", port=7000) 
+
+app.run(host="0.0.0.0", port=7000) 
